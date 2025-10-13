b7fb8a2333d3be78832197883e137071df9bbf51
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-10-13 19:13:44 +0300

    Add Styles class and use custom text style in HomeViewBody
    Introduced a new Styles class in core/utils/styles.dart to centralize text styles. Updated HomeViewBody to use Styles.titleMedium for the 'Best seller' text and set crossAxisAlignment to start for improved layout consistency.

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kitap/core/utils/styles.dart';
 import 'package:kitap/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:kitap/features/home/presentation/views/widgets/featured_books_list.dart';
 
@@ -11,10 +12,13 @@ class HomeViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomAppBar(),
           SizedBox(height: height * 0.02),
           FeaturedBooksList(height: height),
+          SizedBox(height: height * 0.04),
+          Text("Best seller", style: Styles.titleMedium),
         ],
       ),
     );
