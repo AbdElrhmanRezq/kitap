36f879763700c7006b6f71c229fa227c9d9ad9cd
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-10-17 20:25:59 +0300

    Add search text field and results widgets
    Introduces SearchTextField and ResultBooks widgets for the search feature. Updates SearchViewBody to include these widgets in the UI, improving the search view layout.

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:kitap/features/search/presentation/views/widgets/result_books.dart';
+import 'package:kitap/features/search/presentation/views/widgets/search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double height = MediaQuery.of(context).size.height;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+
+      child: Column(
+        children: [SearchTextField(), SizedBox(height: 20), ResultBooks()],
+      ),
+    );
   }
 }
