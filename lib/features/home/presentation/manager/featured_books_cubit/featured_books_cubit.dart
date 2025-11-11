e902399e6881fd9a956b6ba0859c928d5e21fc53
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-11-11 13:25:52 +0200

    Add fetchFeaturedBooks to FeaturedBooksCubit
    Introduced dependency injection of HomeRepo into FeaturedBooksCubit and added fetchFeaturedBooks method to handle loading, success, and failure states when fetching featured books.

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,8 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:kitap/features/home/data/model/book_model/book_model.dart';
+import 'package:kitap/features/home/data/repo/home_repo.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => emit(FeaturedBooksFailure(failure.message)),
+      (books) => emit(FeaturedBooksSuccess(books)),
+    );
+  }
 }
