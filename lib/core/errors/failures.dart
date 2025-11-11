8cdb56e98d588aa4e682a6fa276d2ac7d648f0f1
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-11-11 12:58:01 +0200

    Improve error handling and implement book fetching
    Enhanced the Failure class to include error messages and added detailed error mapping from Dio exceptions. Implemented the fetchBestSellerBooks method in HomeRepoImpl to fetch data using ApiService and handle errors appropriately.

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,50 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String message;
+
+  const Failure(this.message);
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure(super.message);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.cancel:
+        return ServerFailure("Request to server was cancelled");
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure("Connection timeout with server");
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure("Receive timeout in connection with server");
+      case DioExceptionType.sendTimeout:
+        return ServerFailure("Send timeout in connection with server");
+      case DioExceptionType.badResponse:
+        {
+          final statusCode = dioError.response?.statusCode;
+          if (statusCode != null) {
+            switch (statusCode) {
+              case 400:
+                return ServerFailure("Bad request");
+              case 401:
+                return ServerFailure("Unauthorized request");
+              case 403:
+                return ServerFailure("Forbidden request");
+              case 404:
+                return ServerFailure("Not found");
+              case 500:
+                return ServerFailure("Internal server error");
+              default:
+                return ServerFailure(
+                  "Received invalid status code: $statusCode",
+                );
+            }
+          } else {
+            return ServerFailure("Received invalid response from server");
+          }
+        }
+      default:
+        return ServerFailure("Unexpected error occurred");
+    }
+  }
+}
--- lib/features/home/data/repo/home_repo_impl.dart
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -1,12 +1,32 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:kitap/core/errors/failures.dart';
+import 'package:kitap/core/utils/api_service.dart';
 import 'package:kitap/features/home/data/model/book_model/book_model.dart';
 import 'package:kitap/features/home/data/repo/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      var data = await apiService.get(
+        endpoint:
+            'volumes?Filtering=free-ebooks&Sorting=relevance&q=programming',
+      );
+      List<BookModel> books = [];
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 
   @override
