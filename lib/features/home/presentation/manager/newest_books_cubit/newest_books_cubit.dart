ec6266c513a6361210d8dc4f25edcaf259e6c00c
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-11-11 13:29:56 +0200

    Add NewestBooksCubit and state classes
    Introduces NewestBooksCubit for managing the state of fetching the newest books using HomeRepo. Also adds the initial state class for the cubit.

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:kitap/features/home/data/model/book_model/book_model.dart';
+import 'package:kitap/features/home/data/repo/home_repo.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  final HomeRepo homeRepo;
+
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) => emit(NewestBooksFailure(failure.message)),
+      (books) => emit(NewestBooksSuccess(books)),
+    );
+  }
+}
